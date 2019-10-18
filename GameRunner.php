b9eb374ad0fcafe857fcd313a4581dc0df4e317a
Angelo Giuffredi <angelo.giuffredi@docebo.com>
2019-10-18 20:43:54 +0200

    pass

--- GameRunner.php
+++ GameRunner.php
@@ -1,27 +1,25 @@
 <?php
 
-require_once __DIR__.'/Game.php';
+require_once __DIR__ . '/Game.php';
 
-$notAWinner;
+$notAWinner = false;
 
-  $aGame = new Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+
+do {
+
+    $aGame->roll(rand(0, 5) + 1);
+
+    if (rand(0, 9) == 7) {
+        $notAWinner = $aGame->wrongAnswer();
     } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
+        $notAWinner = $aGame->wasCorrectlyAnswered();
     }
-    
-    
-    
-  } while ($notAWinner);
-  
+
+
+} while ($notAWinner);

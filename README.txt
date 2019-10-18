88ab6e3df652e16a2792f262ca075f58611b2368
Andrea Francia <andrea@andreafrancia.it>
2019-10-18 18:08:59 +0200

    Iniital import

---
+++ README.txt
@@ -0,0 +1,6 @@
+Original files from: https://github.com/jbrains/trivia
+
+vendor/bin/phpunit && git add --all && git commit -m "pass" && git push
+
+git pull --rebase || git reset --hard origin/master
+
---
+++ tests/TriviaTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+define("ROOT", realpath(__DIR__ . "/.."));
+
+class TriviaTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        if(!file_exists(pathTo("reference.txt"))) {
+            $this->runManyTimes(pathTo("reference.txt"));
+            self::fail("Refence file created as `reference.txt' please re-run the tests");
+        }
+    }
+
+    function test() {
+        $this->runManyTimes(pathTo("actual.txt"));
+
+        $expected = file_get_contents(pathTo("reference.txt"));
+        $actual = file_get_contents(pathTo("actual.txt"));
+        self::assertEquals($expected, $actual);
+    }
+
+    function runManyTimes($path) {
+        srand(0);
+        ob_start();
+        for($i=0; $i < 50; $i++) {
+            include __DIR__ . "/../GameRunner.php";
+        }
+        $output = ob_get_contents();
+        ob_end_clean();
+        file_put_contents($path, $output);
+    }
+}
+
+function pathTo($basename)
+{
+    return realpath(__DIR__ . "/..") . "/$basename";
+}
